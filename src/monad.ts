a9094bbe64e5a12649ea48e6848a95973e3576f1
Luke <lrsorrenti@hotmail.com>
2023-09-18 21:01:35 -0500

    added first setup to logging

--- src/monad.ts
+++ src/monad.ts
@@ -1,1 +1,26 @@
-console.log(1)
+function square(x: number): number {
+    return x * x;
+}
+
+function addOne(x: number): number {
+    return x + 1;
+} 
+
+console.log(addOne(square(2))) // 5 
+
+// the result of the above looks like: 
+
+/* 
+Call with logging: 
+
+addOne(square(2)) => {
+    result: 5,
+    logs: [
+        "first square 2 to get 4",
+        "second add 1 to 4 to get 5"
+    ]
+}
+*/
+
+// first square 2 to get 4
+// second add 1 to 4 to get 5
